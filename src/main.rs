bc1140fdec9f364ff6d4f34412610a1cbeaf68c3
James Hall <james.hall@shopify.com>
2019-08-22 13:13:43 -0400

    Implement basic file catting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate clap;
+extern crate parquet;
+
+use std::fs::File;
+use std::path::Path;
+use clap::{App, Arg, ArgMatches};
+use parquet::file::reader::{FileReader, SerializedFileReader};
+
 fn main() {
-    println!("Hello, world!");
+    run_app(get_app().get_matches());
+}
+
+fn get_app<'a, 'b>() -> App<'a, 'b> {
+    App::new("parquet-cat")
+        .version("0.1.0")
+        .about("Access data and metadata from parquet files")
+        .arg(Arg::with_name("files")
+            .required(true)
+            .multiple(true))
+}
+
+fn run_app(matches: ArgMatches) {
+    matches.values_of("files").unwrap().for_each(cat_file);
+}
+
+fn cat_file(path: &str) {
+    let file = File::open(&Path::new(path)).unwrap();
+    let reader = SerializedFileReader::new(file).unwrap();
+    let iter = reader.get_row_iter(None).unwrap();
+    iter.for_each(|record| println!("{}", record));
 }
